a351c24746c43a82bde7be05c86f7edfeaa99964
Raz Nik <nikita.razguljajev@gmail.com>
2019-06-15 13:04:44 +0300

    Added /main to controllers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+  	{ title: '30-летие!',
+       locations: { city: 'Tallinn'},
+       alumniList: { name: 'aaa'},
+       alumniView: { username: 'NIKI'}
+   });
+});
+
+router.get('/main', function(req, res, next) {
+  res.render('main', 
+  	{ title: '30-летие!',
+       locations: { city: 'Tallinn'},
+       alumniList: { name: 'aaa'},
+       alumniView: { username: 'NIKI'}
+   });
 });
 
 module.exports = router;
