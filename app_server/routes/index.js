ef09217e06d92af8792ffd03b68f935b10bf5016
dfisun <dfisun@users.noreply.github.com>
2019-11-02 08:12:39 +0200

    Refactor site views and index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,21 +2,7 @@ const router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  	{ title: '30-летие!',
-       locations: { city: 'Tallinn'},
-       alumniList: { name: 'aaa'},
-       alumniView: { username: 'NIKI'}
-   });
-});
-
-router.get('/main', function(req, res, next) {
-  res.render('main', 
-  	{ title: '30-летие!',
-       locations: { city: 'Tallinn'},
-       alumniList: { name: 'aaa'},
-       alumniView: { username: 'NIKI'}
-   });
+  res.render('index');
 });
 
 module.exports = router;
