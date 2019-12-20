6aece75539b207cea48a1aec1ffd3f690066cddd
User <user@example.com>
2019-12-20 13:18:30 +0200

    Switch to Passport.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,13 +2,9 @@ const router = require('express').Router();
 const connect = require('connect-ensure-login');
 
 const authenticationRoutes = require('./authenticationRoutes');
-const userRoutes = require('./userRoutes');
-
-const locations = require('../controllers/locationsController');
 const others = require('../controllers/others');
 
 router.use('/', authenticationRoutes);
-router.use('/users', userRoutes);
 
 /* GET index page. */
 router.get('/map', others.mapPage);
@@ -17,4 +13,5 @@ router.get('/signup', others.signupPage);
 router.get('/login', others.loginPage);
 router.get('/user', connect.ensureLoggedIn(), others.userPage);
 router.get('/', (req, res) => res.redirect('/map'));
+
 module.exports = router;
