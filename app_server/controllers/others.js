b6af9e2ebe033d190687562120f3677131652e0d
User <user@example.com>
2019-12-20 13:36:34 +0200

    Merge

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,4 @@
+<<<<<<< .merge_file_a13576
 
 const mapPage = (req, res) => {
   if (req.user) {
@@ -12,6 +13,48 @@ const mapPage = (req, res) => {
       full: false,      
     })
   };
+=======
+const locations = './api/locations';
+const request = require('request');
+const apiOptions = {
+  server : 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production') {
+  console.log("Server not found");
+}
+
+const renderMap = function(req, res, responseBody) {
+	res.render('map', {
+		title: 'Map',
+        locations: responseBody
+        });
+};
+
+
+const mapList = function(req, res){
+  const path = '/api/locations';
+  const requestOptions = {
+    url : apiOptions.server + path,
+    method : 'GET',
+    json : {},
+    qs : {
+      lng : -0.7992599,
+      lat : 51.378091
+    }
+  };
+  request(
+    requestOptions,
+    (err, response, body) => {
+      let data = body;
+      if (response.statusCode === 200 && data.length) {
+        for (let i = 0; i < data.length; i++) {
+          data[i].distance = _formatDistance(data[i].distance);
+        }
+      }
+      renderMap(req, res, data);
+    }
+  );
+>>>>>>> .merge_file_a00856
 };
 
 const aboutPage = (req, res) => {
@@ -23,14 +66,24 @@ const signupPage = (req, res) => {
 const loginPage = (req, res) => {
   res.render('login', {title: 'Login'})
 };
+<<<<<<< .merge_file_a13576
 const userPage = (req, res) => {
   res.render('user', {title: 'User'})
 };
+=======
+>>>>>>> .merge_file_a00856
 
 module.exports = {
-    mapPage,
+    mapList,
     aboutPage,
+<<<<<<< .merge_file_a13576
     signupPage,
     loginPage,
 	userPage
 };
+=======
+    regPage,
+    loginPage
+};
+
+>>>>>>> .merge_file_a00856
