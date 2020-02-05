2df492746915bb02ffa768b46b30c61c6c594337
Nikita <nikita.razguljajev@gmail.com>
2020-02-05 18:01:31 +0200

    Back to nominatim

--- public/js/address-select-helper.js
+++ public/js/address-select-helper.js
@@ -6,36 +6,41 @@ const selectAddress = (lat1, lng1, lat2, lng2, display_name) => {
   $("#display_name").val(display_name);
 };
 
-const searchAddress = () => {
-  let inp = document.getElementById("addr");
-
-  $.getJSON('https://geocode-maps.yandex.ru/1.x/?format=json&apikey=d61231e6-cea5-435c-9616-8e5f25597fda&geocode=' + inp.value, function (data) {
-    let items = [];
-    let temp = 0;
-
-    let realData = data.response.GeoObjectCollection.featureMember;
-
-    if (realData.length !== 0) {
-      $.each(realData, function (key, val) {
-        let coords = val.GeoObject.Point.pos.split(" ");
-        if (val.GeoObject.metaDataProperty.GeocoderMetaData.kind === "locality") {
-          items.push("<li><a href='#' onclick='selectAddress(" + coords[0] + ", "
-            + coords[1] + ", " + coords[0] + ", " + coords[1] + ", \""
-            + val.GeoObject.metaDataProperty.GeocoderMetaData.text + "\");return false;'>"
-            + val.GeoObject.metaDataProperty.GeocoderMetaData.text + '</a></li>');
-          temp++;
-        }
-      });
-    }
-    if (temp === 0) {
-      items.push("<p><b>Город не найден.</b></p>");
-    } else if (data.length === 0) {
-      items.push("<p><b>Город не найден.</b></p>");
-    }
+function chooseAddr(lat1, lng1, lat2, lng2, display_name) {
     $('#results').empty();
-    $('<ul/>', {
-      'class': 'my-new-list',
-      html: items.join(''),
-    }).appendTo('#results');
-  });
-};
+    $("#realAddr").val(lat1 + "/" +  lng1);
+    $('<b>', { html: "Вы выбрали адрес: " + display_name }).appendTo('#results');
+  $("#lat").val(lat1);
+  $("#lon").val(lng1);
+    $("#display_name").val(display_name);
+  
+}
+function addr_search() {
+    let inp = document.getElementById("addr");
+    
+    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=1&q=' + inp.value, function(data) {
+        let items = [];
+        let temp = 0;
+          if(data.length != 0) {
+        $.each(data, function(key, val) {
+            bb = val.boundingbox;
+                 if(val.type == "administrative" || val.type == "city" || val.type == "town"){
+            items.push("<li><a href='#' onclick='chooseAddr(" + bb[0] + ", " + bb[2] + ", " + bb[1] + ", " + bb[3]  + ", \"" + val.display_name + "\");return false;'>" + val.display_name + '</a></li>');
+            temp++;
+        }
+        });
+    }
+        if(temp == 0) {
+           items.push("<p><b>Город не найден.</b></p>");
+         }
+    else if(data.length == 0){
+         console.log("No location found");
+    items.push("<p><b>Город не найден.</b></p>");
+}
+  $('#results').empty();
+            $('<ul/>', {
+                'class': 'my-new-list',
+                html: items.join('')
+            }).appendTo('#results');
+    });
+}
--- public/js/map.js
+++ public/js/map.js
@@ -1,44 +1,12 @@
-const init = () => {
-	let myMap = new ymaps.Map("map", {
-		center: [59.437411, 24.745181],
-		// zoom (from 0 to 19)
-		zoom: 7,
-		controls: ['zoomControl'],
-	});
-
-  loadLocations().then((locations) => {
-    for (let location of locations) {
-      myMap.geoObjects.add(createPlacemark(location));
-    }
-  });
-}
-
-const loadLocations = async () => {
-  const response = await fetch('/api/locations');
-  const json = await response.json();
-  return json.locations;
-}
-
-const createPlacemark = (location) => {
-  const placemark = new ymaps.Placemark([
-    parseInt(location.lat),
-    parseInt(location.lng)
-  ], {
-    iconContent: location.alumni_count,
-    iconCaption: location.location_name,
-    balloonContent: 'Выпускников: ' + location.alumni_count,
-  }, {
-    preset: 'islands#blueCircleDotIconWithCaption',
-    iconCaptionMaxWidth: '50',
-  });
-
-  placemark.events.add('click', (e) => {
-    console.log(location.location_id, location.location_name);
-  });
-
-  return placemark;
-}
-
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
+$('#alum-view').empty();
+     
+     
+    var mymap = L.map('mapid').setView([58.595, 25.014], 3);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    minZoom:2,
+    id: 'mapbox.streets',
+  
+    accessToken: 'sk.eyJ1Ijoibm9ub2Jpc2hhIiwiYSI6ImNqd201enF3eDF1YnQ0NG8ydTd5cDhkajcifQ.bk0jp5sFLvNJPYTis0Xi_A'
+}).addTo(mymap);
