e37de4ff9427bcad96b386a991ae265e6d4788c4
Nikita <nikita.razguljajev@gmail.com>
2020-02-06 11:54:27 +0200

    Added retryWrite to false

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -3,7 +3,7 @@ const apiOptions = {
   server : 'http://localhost:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-  console.log("Server not found");
+  console.log("production mode");
   apiOptions.server = 'https://tlvl-31.herokuapp.com/';
 }
 
